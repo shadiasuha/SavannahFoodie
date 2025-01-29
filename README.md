4860f38ad99ec01947a89c408108cc50bfa78790
shadiasuha <daniels.shadia@gmail.com>
2025-01-28 20:59:37 -0500

    inital app cleanup and start of dataset

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Three - Savannah Foodie
+
+This is a project I made for FWS..
---
+++ src/data.js
@@ -0,0 +1,27 @@
+const restaurants = [
+
+{
+  restaurantImage:
+  restaurantName:
+  restaurantRating: 
+  streetAddress:
+  cityStateZip: {
+    name:
+    neighborhood:
+  },
+  socialMedia: {
+    facebook:
+    instagram:
+  },
+  directions:
+  phone:
+  hours:
+  cuisineType:
+  popularDishes:
+  menu:
+  diningOptions: 
+  loyaltyProgram:
+}
+]
+
+export default restaurants
